204cbae706d61e3d8a4dc52433f52eaab24af38d
Noam work laptop <noam.almog@nasa.gov>
2014-03-04 15:33:27 -0800

    Initial Commit

---
+++ iCrew/NZAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NZAAppDelegate.h
+//  iCrew
+//
+//  Created by Almog, Noam (ARC-AF)[UNIV OF CALIFORNIA   SANTA CRUZ] on 3/4/14.
+//  Copyright (c) 2014 Almog, Noam (ARC-AF)[UNIV OF CALIFORNIA   SANTA CRUZ]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NZAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iCrew/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iCrew
+//
+//  Created by Almog, Noam (ARC-AF)[UNIV OF CALIFORNIA   SANTA CRUZ] on 3/4/14.
+//  Copyright (c) 2014 Almog, Noam (ARC-AF)[UNIV OF CALIFORNIA   SANTA CRUZ]. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NZAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NZAAppDelegate class]));
+    }
+}
